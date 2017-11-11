70811c0995fca0e85cc343dd572c9a0e3859b173
manrajsingh960 <akalsahai1>
2017-11-11 01:14:02 -0800

    Update as of Sat 11/11 1:13 am

---
+++ app/src/main/java/com/example/manrajsingh960/gettogether/RegisterRequest.java
@@ -0,0 +1,29 @@
+package com.example.manrajsingh960.gettogether;
+
+/**
+ * Created by manrajsingh960 on 11/10/17.
+ */
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterRequest extends StringRequest {
+    private static final String REGISTER_REQUEST_URL = "https://manrajsingh960.000webhostapp.com/Register.php";
+    private Map<String, String> params;
+
+    public RegisterRequest(String username, String password, Response.Listener<String> listener) {
+        super(Method.POST, REGISTER_REQUEST_URL, listener, null);
+        params = new HashMap<>();
+        params.put("username", username);
+        params.put("password", password);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+}
+
